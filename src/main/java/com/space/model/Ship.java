cbb7cea5078a8817f135f2a5581c6b9a9dbc262b
drovocek <mycodepg@gmail.com>
2020-09-14 14:43:34 +0300

    Добавлен репозиторий и модель

---
+++ src/main/java/com/space/model/Ship.java
@@ -0,0 +1,114 @@
+package com.space.model;
+
+import com.space.model.ShipType;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity // This tells Hibernate to make a table out of this class
+//@Table(name = "ship")
+public class Ship {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String planet;
+
+//    @ElementCollection(targetClass = ShipType.class, fetch = FetchType.EAGER)
+//    @CollectionTable(name = "shipType", joinColumns = @JoinColumn(name = "id"))
+    @Enumerated(EnumType.STRING)
+    private ShipType shipType;
+
+    private Date prodDate;
+    private Boolean isUsed;
+    private Double speed;
+    private Integer crewSize;
+    private Double rating;
+
+    public Ship(String name, String planet, ShipType shipType, Date prodDate, Boolean isUsed, Double speed, Integer crewSize, Double rating) {
+        this.name = name;
+        this.planet = planet;
+        this.shipType = shipType;
+        this.prodDate = prodDate;
+        this.isUsed = isUsed;
+        this.speed = speed;
+        this.crewSize = crewSize;
+        this.rating = rating;
+    }
+
+    public Ship() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPlanet() {
+        return planet;
+    }
+
+    public void setPlanet(String planet) {
+        this.planet = planet;
+    }
+
+    public ShipType getShipType() {
+        return shipType;
+    }
+
+    public void setShipType(ShipType shipType) {
+        this.shipType = shipType;
+    }
+
+    public Date getProdDate() {
+        return prodDate;
+    }
+
+    public void setProdDate(Date prodDate) {
+        this.prodDate = prodDate;
+    }
+
+    public Boolean getUsed() {
+        return isUsed;
+    }
+
+    public void setUsed(Boolean used) {
+        isUsed = used;
+    }
+
+    public Double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(Double speed) {
+        this.speed = speed;
+    }
+
+    public Integer getCrewSize() {
+        return crewSize;
+    }
+
+    public void setCrewSize(Integer crewSize) {
+        this.crewSize = crewSize;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+}
